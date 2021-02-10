91f9d1c2ff7bf01af1e29200c53d88e85d206f25
Robert <robertichim98@gmail.com>
2021-02-10 23:18:51 +0000

    Add authentication check

--- src/actions/auth.tsx
+++ src/actions/auth.tsx
@@ -4,7 +4,43 @@ import {
   LOGIN_FAIL,
   USER_LOADED_SUCCESS,
   USER_LOADED_FAIL,
+  AUTHENTICATED_SUCCESS,
+  AUTHENTICATED_FAIL,
+  LOGOUT,
 } from "./types";
+
+export const checkAuthenticated = () => async (dispatch) => {
+  if (localStorage.getItem("access")) {
+    const config = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+
+    const body = JSON.stringify({ token: localStorage.getItem("access") });
+
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/auth/jwt/verify/`,
+        body,
+        config
+      );
+
+      if (res.data.code !== "token_not_valid") {
+        dispatch({
+          type: AUTHENTICATED_SUCCESS,
+        });
+      }
+    } catch (err) {
+      dispatch({
+        type: AUTHENTICATED_FAIL,
+      });
+    }
+  } else {
+    dispatch({
+      type: AUTHENTICATED_FAIL,
+    });
+  }
+};
 
 export const load_user = () => async (dispatch) => {
   if (localStorage.getItem("access")) {
@@ -25,6 +61,7 @@ export const load_user = () => async (dispatch) => {
         type: USER_LOADED_SUCCESS,
         payload: res.data,
       });
+      console.log(LOGIN_SUCCESS);
     } catch (err) {
       dispatch({
         type: USER_LOADED_FAIL,
@@ -66,4 +103,10 @@ export const login = (email, password) => async (dispatch) => {
     });
     console.log(LOGIN_FAIL);
   }
+};
+
+export const logout = () => (dispatch) => {
+  dispatch({
+    type: LOGOUT,
+  });
 };
